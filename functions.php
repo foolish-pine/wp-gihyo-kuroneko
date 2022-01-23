0c9460ee44e3978cb055c27e99f91d44406f25cf
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-01-23 14:54:18 +0900

    6-5

--- functions.php
+++ functions.php
@@ -19,3 +19,26 @@ function neko_enqueue_scripts(){
 	wp_enqueue_style( 'kuroneko-theme-styles', get_template_directory_uri() . '/assets/css/theme-styles.css', array(), '1.0.0' );
 }
 add_action( 'wp_enqueue_scripts', 'neko_enqueue_scripts' );
+
+function neko_widgets_init() {
+	register_sidebar(
+		array(
+			'name' => 'サイドバー',
+			'id' => 'sidebar-widget-area',
+			'description' => '投稿・固定ページのサイドバー',
+			'before_widget' => '<div id="%1$s" class="%2$s">',
+			'after_widget' => '</div>'
+		)
+	);
+	register_sidebars(
+		3,
+		array(
+			'name' => 'フッター %d',
+			'id' => 'footer-widget-area',
+			'description' => 'フッターのサイドバー',
+			'before_widget' => '<div id="%1$s" class="%2$s">',
+			'after_widget' => '</div>'
+		)
+	);
+}
+add_action( 'widgets_init', 'neko_widgets_init' );
